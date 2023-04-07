c12d591e6a457caf244ba77828e2ad35e1ad9923
Dips2020 <dipsbhattarai2020@gmail.com>
2023-04-07 13:12:34 +0545

    deleted example.py

--- Python/python/example.py
+++
@@ -1,8 +0,0 @@
-# hfklasnd,fnsoaidjcn,xc
-dict1 = {'a': 1, 'b': 2}
-dict2 = {'c': 3, 'd': 4}
-dict1.update(dict2)
-print(dict1)
-# Alternatively, merge the two dictionaries using the ** operator
-# merged_dict = {**dict1, **dict2}
-# print(merged_dict)
