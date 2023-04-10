c68927b5a8b23265b777e1a99637ef3846bd4580
Dips2020 <dipsbhattarai2020@gmail.com>
2023-04-10 20:46:47 +0545

    updated and corrected

--- Python/python/6_Tutorial.py
+++ Python/python/6_Tutorial.py
@@ -1,31 +1,31 @@
-# f = open('./text/tutorial6.text', 'r')
-# # print(f.read())
-# print(f.readline())
+# f = open("./Python/txt_file/tutorial_6.txt", "r")
+# print(f.read())
+# # print(f.readline())
 
 
-# to⬇️ open the file outside the folder
-# d = open('../Dipesh bhattarai.txt', 'r')
+# # # to⬇️ open the file outside the main folder
+# d = open('../Dipesh Bhattarai.txt', 'r')
 # print(d.read())
 
-# ⬇️while looping text file
-# file = open('./text/tutorial6.text', 'r')
+# ⬇️while looping txt_file file
+# file = open('./Python/txt_file/tutorial6.txt', 'r')
 # line = file.readline()
 # while line != '':
 #     print(line)
 #     line = file.readline()
 
 # with⬇️ open for loop
-# with open('./text/tutorial6.text', 'r') as file:
+# with open('./Python/txt_file/tutorial6.txt', 'r') as file:
 #     data = file.readlines()
 #     for line in data:
 #         print(line)
 
 # ⬇️write
-# file = open('./text/tutorial6.text', 'w')
+# file = open('./Python/txt_file/tutorial6.txt', 'w')
 # file.write("Dipesh Bhattarai is in Herald College Kathamndu right now. \n")
 
 # append
-# file = open('./text/tutorial6.text', 'a')
+# file = open('./Python/txt_file/tutorial6.txt', 'a')
 # file.write("Dipesh Bhattarai is in Herald College Kathamndu right now. \n")
 
 
@@ -50,9 +50,9 @@
 # print(value)
 
 
-# for data.text ⬇️
+# for datatxt ⬇️
 # checker = ""
-# with open('data.text', 'r') as file:
+# with open('datatxt', 'r') as file:
 #     data = file.readlines()
 #     for line in data:
 #         word = line.split()
---
+++ Python/python/7_Workshop.py
@@ -0,0 +1,5 @@
+# Part 1 ⬇️ 1
+# f = open("./Python/txt_file/7_Workshop.txt", "r")
+# x = f.read()
+# print(x)
+# f.close()
