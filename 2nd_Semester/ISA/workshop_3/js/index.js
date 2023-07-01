299ab4da4d3a7aed11877fba4ecc79a3005bb2a5
Dips2020 <dipsbhattarai2020@gmail.com>
2023-07-01 18:52:43 +0545

    final version rock paper scissor

--- 2nd_Semester/ISA/workshop_3/js/index.js
+++ 2nd_Semester/ISA/workshop_3/js/index.js
@@ -1,76 +1,60 @@
-function playGame(userChoice) {
-    const loader = document.getElementById('loader');
-    const resultCard = document.querySelector('.result-card');
-    const resultMessage = resultCard.querySelector('.result-message');
-
-    // to show the loading
-    loader.style.display = 'block';
-
-    // Hide the result card
-    resultCard.classList.remove('show');
+let winMsg = "!! Victory !!";
+let loseMsg = "!! Crushing Defeat !!";
+let tieMsg = "!! Tie !!";
+let moveList = ["rock", "paper", "scissors"];
 
 
+// this is for loading symbol
+const loadingSymbol = document.querySelector("#loader");
+// this is for result card
+const resultCard = document.querySelector("#result");
+// this is for result message
+const resultMsg = document.querySelector("#result_msg");
+const tryAgainPart = document.querySelector("#tryAgain")
+const mainContainer = document.querySelector("#mainContainer");
 
-    // Delay the result calculation for 1.5 second
+
+function button_click(userChoice) {
+    tryAgainPart.style.display = "block"
+    loadingSymbol.style.display = "block";
+    resultCard.style.display = "none";
+    // result card will appear after 1.5 seconds
     setTimeout(function () {
-        const result = determineWinner(userChoice, computerChoice);
+        loadingSymbol.style.display = "none";
+        resultCard.style.display = "flex";
 
-        // Update the result message
-        resultMessage.textContent = result;
+        // random move for the computer
+        let computerChoice = moveList[Math.floor(Math.random() * moveList.length)];
 
-        // Hide the loading
-        loader.style.display = 'none';
+        console.log("User's choice: " + userChoice);
+        console.log("Computer's choice: " + computerChoice);
 
-        // Show the result card
-        resultCard.classList.add('show');
+        // choosing the winner
+        let result = [];
+        if (userChoice === computerChoice) {
+            result = tieMsg;
+        } else if (
+            (userChoice === "rock" && computerChoice === "scissors") ||
+            (userChoice === "scissors" && computerChoice === "paper") ||
+            (userChoice === "paper" && computerChoice === "rock")
+        ) {
+            result = winMsg;
+        } else {
+            result = loseMsg;
+        }
+
+        // Displaying the choices of user and computer
+        // resultMsg.textContent = `You played: ${userChoice}, Computer played: ${computerChoice}. ${result}`;
+        // !-----⬇️
+        resultMsg.innerHTML = `<span style="font-size:30px;">You played : <span style="color: #6f0000; font-size:30px;">${userChoice}</span></span> <br><span style="font-size:30px;">Computer played: <span style="color: #6f0000; font-size:30px;">${computerChoice}</span> </span> <br><span style="color: #200122; font-size:40px;">${result}</span>`;
     }, 1500);
-}
 
-
-
-// Generate a random choice for the computer
-function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
-}
-// Generate a random choice for the computer
-const computerChoice = getComputerChoice();
-
-let computerMessage = `Computer played: ${computerChoice},Computer Win!`;
-let userMessage = `Computer played: ${computerChoice}, You Win! `
-
-
-
-function determineWinner(userChoice, computerChoice) {
-    if (userChoice === computerChoice) {
-        return "It's a draw!";
-    } else if (
-        (userChoice === 'rock' && computerChoice === 'paper') ||
-        (userChoice === 'paper' && computerChoice === 'scissors') ||
-        (userChoice === 'scissors' && computerChoice === 'rock')
-    ) {
-        return `Computer played: ${computerChoice}, Computer Wins!`;
-    } else {
-        return `Computer played: ${computerChoice}, You Win!`;
+    // for try again part
+    function tryAgain() {
+        resultCard.style.display = "none";
+        mainContainer.style.display = "flex";
     }
+    document.querySelector("#tryAgain").addEventListener("click", tryAgain);
 }
 
 
-// Determine the winner based on the choices
-// function determineWinner(userChoice, computerChoice) {
-//     if (userChoice === computerChoice) {
-//         return "It's a draw!";
-//     } else {
-//         switch (userChoice) {
-//             case 'scissors':
-//                 return computerChoice === 'rock' ? `${computerMessage} \n` : `${userMessage}!`;
-//             case 'paper':
-//                 return computerChoice === 'scissors' ? `${computerMessage}` : `${userMessage}!`;
-//             case 'rock':
-//                 return computerChoice === 'paper' ? `${computerMessage}` : `${userMessage}!`;
-//             default:
-//                 return 'Invalid choice. Please try again!';
-//         }
-//     }
-// }
